331d1dd8ac444aa7f540cc0485ca79f63fb1c593
olferios <olfer.olaya@hotmail.com>
2024-09-03 19:31:48 -0500

    actualizando arquitectura

--- src/main/java/com/proyecto/encuesta/infrastructure/repository/userRol/UserRolRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.encuesta.infrastructure.repository.userRol;
-
-import com.proyecto.encuesta.domain.entities.UserRol;
-import com.proyecto.encuesta.domain.entities.UserRolpk;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRolRepository extends JpaRepository<UserRol, UserRolpk> {
-}
